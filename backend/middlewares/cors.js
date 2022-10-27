5efc1ade8ce57dbcb0f7f9bbea19ae9edc66c2b0
t00l13 <itollik@yandex.ru>
2022-10-27 17:02:34 +0300

    cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,31 +1,22 @@
-const allowedCors = ['http://localhost:7777',
-  'https://localhost:7777',
-  'http://localhost:5000',
+const allowedCors = [
   'https://localhost:5000',
   'https://toolie-mesto.nomoredomains.icu',
   'http://toolie-mesto.nomoredomains.icu',
-  'http://api.toolie-mesto.nomoredomains.icu',
-  'https://api.toolie-mesto.nomoredomains.icu',
 ];
 
 const cors = (req, res, next) => {
   const { origin } = req.headers;
-  // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', true);
   }
   if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
 
