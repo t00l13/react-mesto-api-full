0f31971bce2517a8aef6a9350f7798d3f5f4cef1
t00l13 <itollik@yandex.ru>
2022-11-01 22:31:03 +0300

    asdsd

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -17,7 +17,6 @@ const cors = (req, res, next) => {
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     return res.end();
   }
 
